3493adb2ee2f027b8f2a69b1d35d5575ee50b30f
Ramesh Mhetre <ramesh.mhetre@vegfru.com>
2017-02-24 18:30:35 +0530

    Added textbox enter keypress
    Signed-off-by: Ramesh Mhetre <ramesh.mhetre@vegfru.com>

--- content.js
+++ content.js
@@ -1,12 +1,8 @@
-
-
-
-function startSearching(){
+function startSearching() {
   var links = $("div.g h3.r a").map(function() {
           return this.href;
   }).get();
   var filteredLinks = [];
-  console.log(links);
   $.each(links, function( index, value ) {
     if(3 > filteredLinks.length && value !== undefined) {
         if (~value.indexOf("stackoverflow.com") || ~value.indexOf("quora.com")) {
@@ -28,9 +24,22 @@ function startSearching(){
   $.each(filteredLinks, function(index, link) {
     chrome.runtime.sendMessage({"message": "open_new_tab", "url": link});
   });
-
-
   //chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-   };
+};
 
 window.setTimeout( startSearching, 1800 );
+
+$('#sfdiv button.sbico-c').on('click', function() {
+  window.setTimeout( startSearching, 1800 );
+});
+
+$('#lst-ib').live('keypress', function (e) {
+  alert(e.which);
+         if(e.which === 13){
+
+            //Disable textbox to prevent multiple submit
+            window.setTimeout( startSearching, 1800 );
+
+            //Do Stuff, submit, etc..
+         }
+   });
